9f3fe708cac8f071e8398c80567d5f9d01e9a082
David Tlahuapa <davidtlahuapacoraza.dev@gmail.com>
2022-04-11 09:42:08 -0500

    feat(frontend): Maquetación de aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import TodoList from './components/TodoList';
 const App = () => {
   return (
     <div className="container">
-        <div className="row mt-3">
-            <div className="col-8">
-                <TodoList></TodoList>
-            </div>
-            <div className="col-4">
-                <h1>TodoForm</h1>
-            </div>
+      <div className="row mt-3">
+        <div className="col-8">
+          <TodoList />
         </div>
+        <div className="col-4">
+          <h2>TodoForm</h2>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,34 +3,34 @@ import Todo from './Todo';
 
 const todos = [
     {
-        id: 1,
-        title: 'Aprender React',
-        description: 'Terminar el tutorial práctico de React en YouTube',
-        completed: false
+      id: 1,
+      title: 'Aprender React',
+      description: 'Terminar el tutorial práctico de React en YouTube',
+      completed: false
     },
     {
-        id: 2,
-        title: 'Tareas RAMA',
-        description: 'Atender las tareas pendientes de práctica profesional',
-        completed: true
+      id: 2,
+      title: 'Tareas RAMA',
+      description: 'Atender las tareas pendientes de práctica profesional',
+      completed: true
     },
     {
-        id: 3,
-        title: 'Comer con el chavito',
-        description: 'Regresar a casa y calentar comida para comer',
-        completed: false
+      id: 3,
+      title: 'Comer con el chavito',
+      description: 'Regresar a casa y calentar comida para comer',
+      completed: false
     }
 ];
 
 const TodoList = () => {
-  return (
-    <>
-        <h2 className="text-end">Lista de pendientes</h2>
-        {
-            todos.map(todo => <Todo key={todo.id} todo={todo} />)
-        }
-    </>
-  );
+    return (
+        <>
+            <h2 className="text-end">Lista de pendientes</h2>
+            {
+                todos.map( todo => <Todo key={todo.id} todo={todo}/> )
+            }
+        </>
+    );
 }
 
 export default TodoList;
